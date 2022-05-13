bb9a98ad35b12ef0f31f2a3fcb15b1a2260f72da
Brandon Cullen <brandoncullen2201@gmail.com>
2022-05-12 23:27:59 -0400

    How did I not know about this

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-def createFibSeq():
-    num01 = 0
-    num02 = 1
+from os import system
 
-    for i in range(0, 501):
-        result = num01 + num02
-        num01 = num02
-        num02 = result
-        print(result)
+def main():
+    def createFibSeq():
+        num01 = 0
+        num02 = 1
 
-createFibSeq()
+        for i in range(0, 501):
+            result = num01 + num02
+            num01 = num02
+            num02 = result
+            print(result)
+
+    system("clear")
+    createFibSeq()
+
+if __name__ == "__main__":
+    main()
